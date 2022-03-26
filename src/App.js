fa6bce060605998ba3f339194374eb862e10810b
Nikita Korneev <korolev5526>
2022-03-26 13:32:47 +0700

    complete task 29.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+//29.1
+import React, { useState } from "react";
 
 function App() {
-  return <div>text</div>;
+  const [notes, setNotes] = useState(["a", "b", "c", "d", "e"]);
+  const [editNum, setEditNum] = useState(null);
+
+  const result = notes.map((note, index) => {
+    return (
+      <li key={index}>
+        {note}
+        <button onClick={() => setEditNum(index)}>Edit</button>
+      </li>
+    );
+  });
+
+  function changeHandler(event) {
+    setNotes([
+      ...notes.slice(0, editNum),
+      event.target.value,
+      ...notes.slice(editNum + 1),
+    ]);
+  }
+
+  return (
+    <div>
+      <ul>{result}</ul>
+      <br />
+      <input
+        value={editNum === null ? "" : notes[editNum]}
+        onChange={changeHandler}
+      />
+    </div>
+  );
 }
 
 export default App;
