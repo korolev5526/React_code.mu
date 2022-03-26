ff5cee67de72eb1143ec4a2834134a7fcfe1eaad
Nikita Korneev <korolev5526>
2022-03-26 12:28:04 +0700

    success lesson

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+//28.1
+import React, { useState } from "react";
+
+function getAverage(arr) {
+  let sum = 0;
+
+  for (const elem of arr) {
+    sum += +elem;
+  }
+  return (sum / arr.length).toFixed(2);
+}
 
 function App() {
-  return <div>text</div>;
+  const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  function changeHandler(index, event) {
+    setNotes([
+      ...notes.slice(0, index),
+      event.target.value,
+      ...notes.slice(index + 1),
+    ]);
+  }
+
+  const result = notes.map((note, index) => {
+    return (
+      <input
+        key={index}
+        value={note}
+        onChange={(event) => changeHandler(index, event)}
+      />
+    );
+  });
+
+  return (
+    <div>
+      {result}
+      <br />
+      {getAverage(notes)}
+    </div>
+  );
 }
 
 export default App;
